d92ca147f985057622d330063da63f4fbe8ee2f7
jonie20 <84018132+jonie20@users.noreply.github.com>
2023-11-30 12:29:04 +0300

    Update models.py

--- blogApp/models.py
+++ blogApp/models.py
@@ -6,7 +6,6 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.utils import timezone
-from PIL import Image
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
